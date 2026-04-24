622351f21f103a1f2389497c95c5feffd5d3e2a1
Yogesh Rajure <rajure.yogesh09@gmail.com>
2026-04-25 00:00:28 +0530

    Added home route

--- app.js
+++ app.js
@@ -81,7 +81,9 @@ app.use((req, res, next) => {
     res.locals.currUser   = req.user;
     next();
 });
-
+app.get("/", (req, res) => {
+  res.redirect("/listings");
+});
 app.use('/listing', listingRoutes);
 app.use('/listing/:id/review', reviewRoutes);
 app.use('/', userRoutes);
